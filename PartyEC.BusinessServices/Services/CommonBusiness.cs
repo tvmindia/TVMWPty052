f5417402b49ab38dd087928adf1dd67e98417079
tvm-albert <albertthomson@thrithvam.ae>
2017-03-03 12:31:27 +0530

    Added new contract called icommonbussiness and its implementaion class

---
+++ PartyEC.BusinessServices/Services/CommonBusiness.cs
@@ -0,0 +1,12 @@
+﻿using PartyEC.BusinessServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyEC.BusinessServices.Services
+{
+    public class CommonBusiness:ICommonBusiness
+    {
+    }
+}
