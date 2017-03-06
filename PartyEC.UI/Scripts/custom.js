9839af4064a8ad3e1428f747177ee61715ace03f
tvm-albert <albertthomson@thrithvam.ae>
2017-03-06 17:40:07 +0530

    style changes

--- PartyEC.UI/Scripts/custom.js
+++ PartyEC.UI/Scripts/custom.js
@@ -14,7 +14,13 @@ $(document).ready(function () {
 
         return false;
     });
-
+   
+    $(".dropdown, .btn-group").hover(function () {
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if (dropdownMenu.is(":visible")) {
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
    
 });
 
