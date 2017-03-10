cc9084be58567cceb7f3ac6199b323f5bc05eabc
tvm-albert <albertthomson@thrithvam.ae>
2017-03-10 10:35:09 +0530

    new files added to sollution for master

---
+++ PartyEC.RepositoryServices/Services/MasterRepository.cs
@@ -0,0 +1,12 @@
+﻿using PartyEC.RepositoryServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyEC.RepositoryServices.Services
+{
+    public class MasterRepository: IMasterRepository
+    {
+    }
+}
