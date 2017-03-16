e2c86d799b8be0289bcc790e795f8c1f9e09581b
suvaneeth <suvaneeth@gmail.com>
2017-03-16 13:47:58 +0300

    API controllers added

---
+++ PartyEC.UI/API/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PartyEC.UI.Models;
+
+namespace PartyEC.UI.API
+{
+    public class CategoryController : ApiController
+    {
+
+        
+    }
+}
