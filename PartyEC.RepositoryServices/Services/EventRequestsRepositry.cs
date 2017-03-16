a12ae53a079a406058547287437cb1df985aa676
tvm-gibin <gibin.jacob@thrithvam.me>
2017-03-16 14:55:18 +0530

    new module Added-Event Request

---
+++ PartyEC.RepositoryServices/Services/EventRequestsRepositry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyEC.RepositoryServices.Services
+{
+    public class EventRequestsRepositry
+    {
+    }
+}
