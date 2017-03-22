1f81bb6ec7eebb56df854a31ba45b9de2f1d4c89
tvm-gibin <gibin.jacob@thrithvam.me>
2017-03-22 15:27:07 +0530

    Cart_Wishlist: controller,ViewModel &view files added

---
+++ PartyEC.UI/Controllers/Cart_WishlistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyEC.UI.Controllers
+{
+    public class Cart_WishlistController : Controller
+    {
+        // GET: Cart_Wishlist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
