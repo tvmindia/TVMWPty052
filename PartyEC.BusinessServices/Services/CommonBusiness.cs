595c9c4799fdeffd29f19efd41688431bcf34f74
suvaneeth <suvaneeth@gmail.com>
2017-03-23 16:00:52 +0300

    OTP msglogic added

--- PartyEC.BusinessServices/Services/CommonBusiness.cs
+++ PartyEC.BusinessServices/Services/CommonBusiness.cs
@@ -3,7 +3,10 @@ using PartyEC.DataAccessObject.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.UI.WebControls;
+using System.IO;
 
 namespace PartyEC.BusinessServices.Services
 {
@@ -36,6 +39,37 @@ namespace PartyEC.BusinessServices.Services
             DateTime DateNow = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
             return (TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateNow, tz));
         }
-      
+
+
+
+        public static void SendMessage(string Msg, string MobileNos)
+        {
+            string[] IndividualMsgs = Msg.Split('|');
+            string[] IndividualMobileNos = MobileNos.Split('|');
+            foreach (var msg in IndividualMsgs) //msg is individual message
+            {
+                foreach (var Num in IndividualMobileNos) //Num is individual Number
+                {
+                    if (Num != string.Empty)
+                    {
+                        if (msg != string.Empty)
+                        {
+                            string strUrl = "http://api.mVaayoo.com/mvaayooapi/MessageCompose?user=shamilatps5@gmail.com:123456&senderID=TEST SMS&receipientno=" + Num + "&msgtxt=" + msg + "&state=4";
+                            WebRequest request = HttpWebRequest.Create(strUrl);
+                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                            Stream s = (Stream)response.GetResponseStream();
+                            StreamReader readStream = new StreamReader(s);
+                            string dataString = readStream.ReadToEnd();
+                            response.Close();
+                            s.Close();
+                            readStream.Close();
+
+                        }
+                    }
+                }
+
+            }
+        }
+
     }
 }
