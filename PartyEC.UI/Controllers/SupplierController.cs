fbc0fff0c601fdcd1a4f6597e51ab19e00862ca7
tvm-gibin <gibin.jacob@thrithvam.me>
2017-03-31 10:15:07 +0530

    suppliers js added

--- PartyEC.UI/Controllers/SupplierController.cs
+++ PartyEC.UI/Controllers/SupplierController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using PartyEC.BusinessServices.Contracts;
+using PartyEC.DataAccessObject.DTO;
+using PartyEC.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,38 @@ namespace PartyEC.UI.Controllers
 {
     public class SupplierController : Controller
     {
+
+        #region Constructor_Injection 
+
+        IMasterBusiness _masterBusiness;
+        ICommonBusiness _commonBusiness;
+
+        public SupplierController(IMasterBusiness masterBusiness, ICommonBusiness commonBusiness)
+        {
+            _commonBusiness = commonBusiness;
+            _masterBusiness = masterBusiness;
+        }
+        #endregion Constructor_Injection 
         // GET: Supplier
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpGet]
+        public string GetAllSuppliers()
+        {
+            try
+            {               
+                List<SupplierViewModel> supplierList = Mapper.Map<List<Supplier>, List<SupplierViewModel>>(_masterBusiness.GetAllSuppliers());
+                return JsonConvert.SerializeObject(new { Result = "OK", Records = supplierList });
+            }
+            catch (Exception ex)
+            {
+                return JsonConvert.SerializeObject(new { Result = "ERROR", Message = ex.Message });
+            }
+
         }
     }
 }
