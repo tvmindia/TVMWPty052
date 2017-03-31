8a73edb3041deb03fcad0b9336c8e99add53a5bb
tvm-gibin <gibin.jacob@thrithvam.me>
2017-03-31 12:22:35 +0530

    jd bundle supplier

--- PartyEC.UI/App_Start/BundleConfig.cs
+++ PartyEC.UI/App_Start/BundleConfig.cs
@@ -49,7 +49,7 @@ namespace PartyEC.UI.App_Start
             bundles.Add(new ScriptBundle("~/bundles/Order").Include("~/Scripts/UserJS/Order.js"));
             bundles.Add(new ScriptBundle("~/bundles/AssoProduct").Include("~/Scripts/UserJS/AssociatedProduct.js"));
             bundles.Add(new ScriptBundle("~/bundles/CartWishlist").Include("~/Scripts/UserJS/Cart_Wishlist.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/Suppliers").Include("~/Scripts/UserJS/Suppliers.js"));
         }
     }
 }
