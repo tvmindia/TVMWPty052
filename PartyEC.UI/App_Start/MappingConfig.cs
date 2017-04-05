7c74c5466eb0d78ceaf65707af0ecf7f7c9113d9
tvm-gibin <gibin.jacob@thrithvam.me>
2017-04-05 12:09:13 +0530

    review business,repository files added

--- PartyEC.UI/App_Start/MappingConfig.cs
+++ PartyEC.UI/App_Start/MappingConfig.cs
@@ -43,7 +43,7 @@ namespace PartyEC.UI.App_Start
                 config.CreateMap<ProductReviewViewModel, ProductReview>().ReverseMap();
                 config.CreateMap<OtherImagesViewModel, OtherImages>().ReverseMap();
                 config.CreateMap<ShippingLocationViewModel, ShippingLocations>().ReverseMap();
-                config.CreateMap<SupplierLocationsViewModel, SupplierLocations>().ReverseMap();
+                config.CreateMap<SupplierLocationsViewModel, SupplierLocations>().ReverseMap(); 
             });
         }
 
