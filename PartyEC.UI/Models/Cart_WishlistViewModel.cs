7e25475a258382ba46a5790464de86ed31d72485
tvm-gibin <gibin.jacob@thrithvam.me>
2017-04-19 16:45:23 +0530

    customer Api Controller added

--- PartyEC.UI/Models/Cart_WishlistViewModel.cs
+++ PartyEC.UI/Models/Cart_WishlistViewModel.cs
@@ -30,4 +30,28 @@ namespace PartyEC.UI.Models
 
        public List<AttributeValues> AttributeValues { get; set; }
     }
+    public class Cart_WishlistAppViewModel
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string CustomerName { get; set; }
+        public string ProductSpecXML { get; set; }
+        public int Qty { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal CurrencyRate { get; set; }
+        public decimal Price { get; set; }
+        public string ItemStatus { get; set; }
+        public string CreatedDate { get; set; }
+
+        public int CartCount { get; set; }
+        public int WishCount { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string DaysinWL { get; set; }
+
+        public List<AttributeValues> AttributeValues { get; set; }
+    }
 }
