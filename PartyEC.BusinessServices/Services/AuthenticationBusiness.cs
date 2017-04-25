257f73790b252eeb506cf285dc9aa4c8952177c6
tvm-albert <albertthomson@thrithvam.ae>
2017-04-25 15:00:35 +0530

    new layer files added

---
+++ PartyEC.BusinessServices/Services/AuthenticationBusiness.cs
@@ -0,0 +1,13 @@
+﻿using PartyEC.BusinessServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyEC.BusinessServices.Services
+{
+    public class AuthenticationBusiness: IAuthenticationBusiness
+    {
+
+    }
+}
---
+++ PartyEC.RepositoryServices/Services/AuthenticationRepository.cs
@@ -0,0 +1,13 @@
+﻿using PartyEC.RepositoryServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyEC.RepositoryServices.Services
+{
+    public class AuthenticationRepository: IAuthenticationRepository
+    {
+
+    }
+}
