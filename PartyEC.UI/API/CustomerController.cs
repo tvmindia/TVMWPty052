1326956d5a53bcf3f42de75ad7412ef18259a004
tvm-gibin <gibin.jacob@thrithvam.me>
2017-05-03 12:48:02 +0530

    small change in api parameter

--- PartyEC.UI/API/CustomerController.cs
+++ PartyEC.UI/API/CustomerController.cs
@@ -68,7 +68,7 @@ namespace PartyEC.UI.API
         {
             try
             {
-                List<Cart_WishlistAppViewModel> CartList = Mapper.Map<List<Wishlist>, List<Cart_WishlistAppViewModel>>(_cartwishlistBusiness.GetCustomerWishlist(WishlistObj.ID));
+                List<Cart_WishlistAppViewModel> CartList = Mapper.Map<List<Wishlist>, List<Cart_WishlistAppViewModel>>(_cartwishlistBusiness.GetCustomerWishlist(WishlistObj.CustomerID));
                 if (CartList.Count == 0) throw new Exception(constants.NoItems);
                 return JsonConvert.SerializeObject(new { Result = true, Records = CartList });
             }
