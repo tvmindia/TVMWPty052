453c68e660c5fbed551e192acd71eefb723b09d2
tvm-gibin <gibin.jacob@thrithvam.me>
2017-05-06 12:04:36 +0530

    image url property bookingsappviewmodel

--- PartyEC.UI/Models/BookingsViewModel.cs
+++ PartyEC.UI/Models/BookingsViewModel.cs
@@ -78,6 +78,7 @@ namespace PartyEC.UI.Models
         public string BillStateProvince { get; set; }
         public string BillContactNo { get; set; }
         public string ProductName { get; set; }
+        public string ImageUrl { get; set; }
 
         //Customer Details
         public CustomerViewModel customerObj { get; set; }
